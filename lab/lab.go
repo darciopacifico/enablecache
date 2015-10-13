fbcae568418f41666bd3d78877b4d156481eb104
Darcio Pacifico <darcio.pacifico@walmart.com>
2015-10-13 00:15:09 -0300

    refactorings

--- lab/lab.go
+++ lab/lab.go
@@ -1,32 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"github.com/golang/groupcache"
 )
 
 func main() {
 
-	thumb := groupcache.NewGroup("dlp", 64<<20, groupcache.GetterFunc(
-		func(ctx groupcache.Context, key string, dest groupcache.Sink) error {
-			fileName := key
-			dest.SetBytes(generateThumb(fileName))
-			return nil
-		}))
-
-	groupcache.RegisterServerStart(func() { fmt.Println("chamando funcao q nao sei pra q eh...") })
-
-	var data []byte
-	thumb.Get(nil, "product:123", groupcache.AllocatingByteSliceSink(&data))
-
-	fmt.Println(fmt.Sprintf("resultado retornado pelo get: %v", string(data)))
-
 }
 
-func generateThumb(fileName string) []byte {
-
-	fmt.Println("gerando bytes para serem cacheados")
-
-	return []byte("Um array de bytes para img ... " + fileName)
-
-}
